371146b4593f5d318e86dcb88bd9de18f23cc95e
SaadZellag <saad.zellag@gmail.com>
2022-01-22 19:49:21 -0500

    added helloworld and simple test to make sure everything works

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @Test
+    public void testAssertion() {
+        assertEquals(4, 2 + 2);
+    }
+}
