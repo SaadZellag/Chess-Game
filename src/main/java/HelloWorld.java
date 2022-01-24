e6175573260335a1eff34bea00fa0ede4d7c52f6
Zonum9 <ganonx9@gmail.com>
2022-01-24 15:32:42 -0500

    Added javafx requirements to the build.gradle file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,14 @@
-public class HelloWorld {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
     public static void main(String[] args) {
         System.out.println("This Project Works");
+        Application.launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.show();
     }
 }
