d57f57f0ba6432c33cf7b4d7edf2332fe3514f18
SaadZellag <saad.zellag@gmail.com>
2022-03-01 17:22:40 -0500

    Fixed issue with ConfidenceBar not binding properly

--- src/main/java/GUI/GameScene/ConfidenceBar.java
+++ src/main/java/GUI/GameScene/ConfidenceBar.java
@@ -1,15 +1,15 @@
 package GUI.GameScene;
 
-import javafx.animation.*;
+import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.geometry.Pos;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.util.Duration;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ConfidenceBar extends GridPane {
     StackPane  stackPane= new StackPane();
@@ -17,39 +17,43 @@ public class ConfidenceBar extends GridPane {
     Rectangle top = new Rectangle(19,600, Color.BLACK);
     Rectangle bottom = new Rectangle(19,600,Color.WHITE);
 
-    ConfidenceBar(ReadOnlyDoubleProperty binding,boolean whiteIsBottom){
+    double percentage = 0.5;
+
+    static final long REFRESH_RATE = 120;
+    // Recommended 0 (non-inclusive) and 1 to determine how fast the rectangle updates
+    // Can be set over 1, but it will be very slow
+    static final double GROWTH_DELAY = 0.5;
+
+
+    ConfidenceBar(ReadOnlyDoubleProperty binding, boolean whiteIsBottom){
         if (!whiteIsBottom){
             Rectangle temp=top;
             top=bottom;
             bottom=temp;
         }
         prefSizePropertyBind(binding);
+
         top.heightProperty().bind(binding.divide(2.2));
         stackPane.setAlignment(Pos.TOP_CENTER);
         stackPane.getChildren().addAll(bottom,top); //this order will have to depend on which player is at the top
         getChildren().add(stackPane);
 
         setAlignment(Pos.CENTER);
-        setOnMouseClicked(e->moveBar(Math.random()));
 
-
-    }
-    public void moveBar(double percentage){
-        Thread t = new Thread(()->{
-            top.heightProperty().unbind();
-            double heightDifference= bottom.getHeight()*percentage-top.getHeight();
-            for (double i = 0; i < 200; i++) {
-                try {
-                    top.setHeight(top.getHeight()+heightDifference/200);
-                    Thread.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Timer timer = new Timer(true);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                double currentPercentage = top.getHeight()/bottom.getHeight();
+                double delta = (percentage - currentPercentage) / REFRESH_RATE * Math.abs(1 / GROWTH_DELAY);
+                Platform.runLater(() -> top.heightProperty().bind(bottom.heightProperty().multiply(currentPercentage+delta)));
             }
-            top.heightProperty().bind(bottom.heightProperty().multiply(percentage));
-        });
-        t.start();
+        }, 0, 1000 / REFRESH_RATE);
+
+
+        setOnMouseClicked(e-> this.percentage = Math.random());
     }
+
     public void prefSizePropertyBind (ReadOnlyDoubleProperty binding){
         bottom.heightProperty().bind(binding.divide(1.1));
         top.yProperty().bind(bottom.yProperty());
