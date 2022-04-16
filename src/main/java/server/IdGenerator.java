d8c1eabfbc6709692765e85d8ee91ccb5ffa857c
Alex Lamarche <alexandre_perso@outlook.com>
2022-04-16 18:59:55 -0400

    Whooops forgot to add id generation class

---
+++ src/main/java/server/IdGenerator.java
@@ -0,0 +1,62 @@
+package server;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IdGenerator {
+
+    /*
+    Usage:
+
+    IdGenerator gen = new IdGenerator();
+    gen.ipToId("192.168.1.44"); [returns azure buckaroo]
+
+     */
+
+    String[] wordArray;
+    String ip;
+
+    public IdGenerator() {
+        try {
+            File wordList = new File("src/main/resources/server/words.txt");
+            Scanner reader = new Scanner(wordList);
+            StringBuilder sb = new StringBuilder();
+            while (reader.hasNext()) {
+                sb.append(reader.nextLine());
+                wordArray = sb.toString().split(",");
+            }
+
+            //Gets local IP, not loopback address
+            Socket s = new Socket("www.google.com", 80);
+            ip = s.getLocalAddress().getHostAddress();
+            s.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String[] getWordArray() {
+        return wordArray;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    /*
+    Takes last two bytes of ip and indexes into words list to
+    generate a more readable hostname.
+     */
+    public String ipToID(String ip) {
+        ArrayList<Integer> bytes = new ArrayList<>();
+        for (String s : ip.split("\\.")) {
+            bytes.add(Integer.parseInt(s));
+        }
+        return wordArray[bytes.get(2)] + " " + wordArray[bytes.get(3)];
+    }
+}
