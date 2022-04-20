70ae4e409fcdf57c899b682e42ac0ca9a5aedaaa
SaadZellag <saadzellagui.wb@gmail.com>
2022-04-20 11:10:27 -0400

    bug fix with the Engine and the dll

--- src/main/java/engine/Engine.java
+++ src/main/java/engine/Engine.java
@@ -8,6 +8,7 @@ import game.Piece;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -86,7 +87,7 @@ public class Engine {
     }
 
     private static void checkSearchStatus() {
-        if (currentSearch != null && !(currentSearch.isCancelled() | currentSearch.isDone())) {
+        if (currentSearch != null && !currentSearch.isDone()) {
             throw new IllegalCallerException("Cannot call another search while one is pending");
         }
     }
